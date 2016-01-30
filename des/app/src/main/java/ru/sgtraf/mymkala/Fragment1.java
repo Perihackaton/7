8622bdf8b2d6bad58b198f8d5321db7ce7b3d51d
Gad <spam@vitoelexir.ru>
2016-01-30 21:37:51 +0300

    Added layout of the main activity

---
+++ des/app/src/main/java/ru/sgtraf/mymkala/Fragment1.java
@@ -0,0 +1,19 @@
+package ru.sgtraf.mymkala;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gad on 30.01.2016.
+ */
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, null);
+    }
+}
